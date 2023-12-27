778e23bb92ca6862d4c56e0113d17bb73c4f240a
EwenAnderson19 <ewenhanderson@hotmail.com>
2023-12-27 12:56:30 +0100

    Attempt to implement ORS routing

--- CyclingRoutePlanner/RoutePlanner.js
+++ CyclingRoutePlanner/RoutePlanner.js
@@ -1,8 +1,4 @@
-//Initialise map
-//var map = L.map('map').setView([50.4, 8.8], 5);
-
-
-//Add tile layer
+//Add tile layers
 //check OSM Tile Usage Policy before using this tile in prod
 
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -18,31 +14,53 @@ var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles
 });
 
 
+//set map bounds
+var southWest = L.latLng(-89.98155760646617, -250),
+northEast = L.latLng(89.99346179538875, 250);
+var bounds = L.latLngBounds(southWest, northEast);
 
-//CartoDB_Voyager.addTo(map)
 
-
-var myIcon = L.icon({
-    iconUrl: '/CyclingRoutePlanner/Images/MountainIcon.png',
+//set climb icon
+var climbIcon = L.icon({
+    iconUrl: '/Images/MountainIcon.png',
     iconSize: [40, 40],
     iconAnchor: [20, 20],
     popupAnchor: [0, -20],
 });
 
-var ColDuVam = L.marker([52.791089, 6.524524], {icon: myIcon}).bindPopup('This is the Col du Vam').openPopup();
+//Import climbs data
+var climbscluster = L.markerClusterGroup();
 
-var climbs = L.layerGroup([ColDuVam]);
+var climbsData = L.geoJson(climbsJSON, 
+    {
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup(feature.properties.name)},
+    pointToLayer: function(feature, latlng) {
+        return L.marker(latlng, {icon: climbIcon})}
+    })
 
-//climbs.addTo(map)
+climbsData.addTo(climbscluster)    
 
 
-//layer control
+//create climbs layer group
+var climbs = L.layerGroup([climbscluster]);
+
+
+//initialise map
 var map = L.map('map', {
     center: [50.4, 8.8],
     zoom: 6,
-    layers: [osm, climbs]
+    layers: [CartoDB_Voyager], //layers: [CartoDB_Voyager, climbs]
+    
 });
 
+map.setMaxBounds(bounds);
+map.on('drag', function() {
+    map.panInsideBounds(bounds, { animate: false }); //set map boundaries
+});
+map.options.minZoom = 2; //set min zoom
+
+//layer control
 var baseMaps = {
     "OpenStreetMap": osm,
     "CartoDBVoyager": CartoDB_Voyager
@@ -53,6 +71,36 @@ var overlayMaps = {
 };
 
 var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+//Initialising ORS
+const Myapikey = "5b3ce3597851110001cf6248f4b8ded8bdf44b46bcb32ea1d9b1a2c4";
+
+
+// L.Routing.control({
+//     waypoints: [
+//         L.latLng(46.2, 6.15),
+//         L.latLng(45.9, 6.8)
+//     ],
+//     router: new L.Routing.openrouteserviceV2("5b3ce3597851110001cf6248f4b8ded8bdf44b46bcb32ea1d9b1a2c4",
+//         {
+//             profile: "driving-car",
+//             geometry_simplify: true // Needs to do _decodePolyline and smaller request
+//         }
+//     )
+// }).addTo(map);
+
+
+L.Routing.control({
+    waypoints: [
+        L.latLng(46.2, 6.15),
+        L.latLng(45.9, 6.8)
+    ],
+    router: new L.Routing.openrouteserviceV2("5b3ce3597851110001cf6248f4b8ded8bdf44b46bcb32ea1d9b1a2c4", 
+    {
+      profile: "driving-car",
+    }),
+  }).addTo(map);
+
 
 //Adding map click event function
 var popup = L.popup();
@@ -65,6 +113,7 @@ function onMapClick(e) {
 }
 
 map.on('click', onMapClick);
+
 
 
 //remember to access the console by 'command, option, J'. We can wrte log files to here using console.log()
